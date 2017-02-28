ff8271e316530bccfa88f825c06e5e64df753b11
hongxudong <hongxudong@huoniu360.com>
2017-03-01 07:52:43 +0800

    修修改demo

--- NettyDemo/src/main/java/org/tcdd/netty/hello/NettyServerHello.java
+++ NettyDemo/src/main/java/org/tcdd/netty/hello/NettyServerHello.java
@@ -7,6 +7,7 @@ import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
 import org.jboss.netty.channel.Channels;
+import org.jboss.netty.handler.codec.string.StringDecoder;
 import org.jboss.netty.handler.codec.string.StringEncoder;
 
 import java.net.InetSocketAddress;
@@ -21,10 +22,11 @@ import java.util.concurrent.Executors;
 public class NettyServerHello {
 
     public static void main(String[] args) {
-
+        //创建服务器的启动器
         ServerBootstrap bootstrap = new ServerBootstrap();
-
+        //创建线程池，链接管理的线程池
         ExecutorService boss = Executors.newCachedThreadPool();
+        //IO传输的连接池
         ExecutorService worker = Executors.newCachedThreadPool();
         bootstrap.setFactory(new NioServerSocketChannelFactory(boss,worker));
         bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
@@ -32,12 +34,15 @@ public class NettyServerHello {
             @Override
             public ChannelPipeline getPipeline() throws Exception {
                 ChannelPipeline pipeline = Channels.pipeline();
-                //pipeline.addLast("encoder", new StringEncoder());
+                //设置解码
+                pipeline.addLast("decoder", new StringDecoder());
+                //设置编码
+                pipeline.addLast("encoder", new StringEncoder());
                 pipeline.addLast("handle", new ServerHandle());
                 return pipeline;
             }
         });
-
+        //绑定端口
         bootstrap.bind(new InetSocketAddress(8888));
         System.out.println("server started...");
     }
--- NettyDemo/src/main/java/org/tcdd/netty/hello/ServerHandle.java
+++ NettyDemo/src/main/java/org/tcdd/netty/hello/ServerHandle.java
@@ -8,30 +8,61 @@ import org.jboss.netty.channel.*;
  */
 public class ServerHandle extends SimpleChannelHandler {
 
-
+    /**
+     * 收到消息时执行
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
     @Override
     public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
 //        String msg = (String) e.getMessage();
-        ChannelBuffer buffer = (ChannelBuffer) e.getMessage();
-        String msg = new String(buffer.array());
+        String msg = (String) e.getMessage();
         System.out.println("messageReceived:"+msg);
+        ctx.getChannel().write("this is ok..\n");
     }
 
+    /**
+     * 出现异常时显示
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
         System.out.println("exceptionCaught");
+        Throwable throwable =  e.getCause();
+        System.out.println(throwable.getMessage());
     }
 
+    /**
+     * 渠道socket连接建立的时候
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
     @Override
     public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
         System.out.println("channelConnected");
     }
 
+    /**
+     * 连接关闭的时候，只有当连接建立之后才会调用
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
     @Override
     public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
         System.out.println("channelDisconnected");
     }
 
+    /**
+     * 连接释放，释放资源。无论连接是否成功，都有调用
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
     @Override
     public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
         System.out.println("channelClosed");
