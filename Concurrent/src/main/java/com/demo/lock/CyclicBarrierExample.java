5724570c4f2bf712e95dc67756a7770682535284
hongxudong <hongxudong@huoniu360.com>
2017-04-05 23:03:47 +0800

    添加 CountDownLatch 和 CyclicBarrier “d小例子

---
+++ Concurrent/src/main/java/com/demo/lock/CyclicBarrierExample.java
@@ -0,0 +1,75 @@
+package com.demo.lock;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Created by ZBOOK-17 on 2017/4/5.
+ */
+public class CyclicBarrierExample {
+
+    public static void main(String[] args) {
+
+        final Random random = new Random();
+
+        final CyclicBarrier cb = new CyclicBarrier(3);
+
+        System.out.println("所有选手准备好了吗？？？");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(random.nextInt(5) * 1000);
+                    System.out.println(Thread.currentThread().getName() + " 我准备好啦..." );
+
+                    cb.await();
+                    System.out.println(Thread.currentThread().getName() + " 我开始跑啦啦啦..." );
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(random.nextInt(5) * 1000);
+                    System.out.println(Thread.currentThread().getName() + " 我准备好啦..." );
+
+                    cb.await();
+                    System.out.println(Thread.currentThread().getName() + " 我开始跑啦啦啦..." );
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(random.nextInt(5) * 1000);
+                    System.out.println(Thread.currentThread().getName() + " 我准备好啦..." );
+
+                    cb.await();
+                    System.out.println(Thread.currentThread().getName() + " 我开始跑啦啦啦..." );
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+    }
+}
