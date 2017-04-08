f99db167c0fc544c561f45cc643a921843e6e63a
xudong.hong <xudong.hong@zhulebei.com>
2017-04-08 22:26:51 +0800

    使用ReentrantLock 的Condition条件，实现一个简单的堵塞队列

--- Concurrent/src/main/java/com/demo/lock/LockConditionExample.java
+++ Concurrent/src/main/java/com/demo/lock/LockConditionExample.java
@@ -1,40 +1,57 @@
 package com.demo.lock;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 /**
  * @Author: hongxudong@lxfintech.com
- * @Created: 2017/4/7 上午1:06
+ * @Created: 2017/4/8 下午9:48
  * @Description: //TODO
  */
 
 public class LockConditionExample {
 
-    private Lock locks = new ReentrantLock();
-    private Condition empty = locks.newCondition();
-    private Condition notEmpty = locks.newCondition();
-
-    private volatile List<String>  list = new ArrayList<>();
-
-    public void put(String str){
-        //先判断大小
-        list.size();
-        //插入
-    }
-
-    public String take(){
-
-        return null;
-    }
-
-
     public static void main(String[] args) {
 
-    }
+        final ProductQueue<String> queue = new ProductQueue<>();
 
+        Thread s = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 15; i++) {
+                    System.out.println(Thread.currentThread().getName()+ "  添加元素：  Task"+i);
+                    queue.put("Task"+i);
+                }
+            }
+        },"t1");
+        s.start();
+
+        Thread s2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for ( ; ; ) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName()+ "  取走元素：  " + queue.take());
+                }
+            }
+        },"t2");
+        s2.start();
+
+        Thread s3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for ( ; ; ) {
+                    try {
+                        Thread.sleep(600);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName()+ "  取走元素：  " + queue.take());
+                }
+            }
+        },"t3");
+        s3.start();
+    }
 
 }
---
+++ Concurrent/src/main/java/com/demo/lock/ProductQueue.java
@@ -0,0 +1,85 @@
+package com.demo.lock;
+
+import com.sun.org.apache.xerces.internal.xs.datatypes.ObjectList;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author: hongxudong@lxfintech.com
+ * @Created: 2017/4/8 下午9:49
+ * @Description:  一个使用 ReentrantLock、Condition 实现的简单FIFO 堵塞队列
+ */
+
+public class ProductQueue<T> {
+    //重入锁
+    private ReentrantLock lock = new ReentrantLock();
+    //当队列中满了的时候，该条件成立，等待其他线程取数据
+    private Condition notFull = lock.newCondition();
+    //当队列中空了的时候，该条件成立，等待其他线程存放数据
+    private Condition notEmpty = lock.newCondition();
+
+    private LinkedList<T> list = new LinkedList<T>();
+
+    private volatile int count;
+    private int max;
+
+
+    public ProductQueue(){
+        max = 10;
+    }
+
+    public ProductQueue(int max){
+        this.max = max;
+    }
+
+    public void put(T t){
+        try {
+            //获取锁
+            lock.lock();
+
+            //如果队列满了，则等待，直到有其他线程调用take方法移除元素，队列有空闲空间之后，才能唤醒当前线程继续执行
+            while (count >= max)
+                notFull.await();
+
+            count++;
+            //添加任务到最后
+            list.addLast(t);
+            //唤醒其他等待的线程：当队列为空的时候，调用take的线程会堵塞住，直到这里调用notEmpty条件唤醒
+            notEmpty.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public T take(){
+        T o = null;
+        try{
+            //获取锁
+            lock.lock();
+
+            //如果队列为空，则等待，直到有其他线程调用put方法添加元素之后，才能唤醒当前线程继续执行
+            while (count==0)
+                notEmpty.await();
+            count--;
+            //取出队列第一个元素 并且返回
+            o = list.removeFirst();
+
+            //唤醒其他等待的线程： 当队列满的时候，调用put方法的线程就会堵塞住，直到这里调用notFull条件唤醒
+            notFull.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+        return o;
+    }
+
+    public int size(){
+        return count;
+    }
+}
