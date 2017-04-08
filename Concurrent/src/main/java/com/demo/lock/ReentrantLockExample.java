c963981d0c8d2e38971be3c68c33528d6389d848
xudong.hong <xudong.hong@zhulebei.com>
2017-04-08 20:52:03 +0800

    添加 重入锁和读写锁的demo

--- Concurrent/src/main/java/com/demo/lock/ReentrantLockExample.java
+++ Concurrent/src/main/java/com/demo/lock/ReentrantLockExample.java
@@ -24,6 +24,8 @@ public class ReentrantLockExample {
         try {
             lock.lock();
             System.out.println(Thread.currentThread().getName() + " : method1 进入");
+            System.out.println(Thread.currentThread().getName() + " : method1 condition1 等待");
+            Thread.sleep(1000);
             condition1.await();
             System.out.println(Thread.currentThread().getName() + " : method1 退出");
         } catch (InterruptedException e) {
@@ -37,6 +39,8 @@ public class ReentrantLockExample {
         try {
             lock.lock();
             System.out.println(Thread.currentThread().getName() + " : method2 进入");
+            System.out.println(Thread.currentThread().getName() + " : method2 condition2 等待");
+            Thread.sleep(1000);
             condition2.await();
             System.out.println(Thread.currentThread().getName() + " : method2 退出");
         } catch (InterruptedException e) {
@@ -50,6 +54,7 @@ public class ReentrantLockExample {
         try {
             lock.lock();
             System.out.println(Thread.currentThread().getName() + " : method3 进入");
+            System.out.println(Thread.currentThread().getName() + " : method3 condition1 等待");
             condition1.await();
             System.out.println(Thread.currentThread().getName() + " : method3 退出");
         } catch (InterruptedException e) {
@@ -62,8 +67,11 @@ public class ReentrantLockExample {
     public void method4(){
         try {
             lock.lock();
-            System.out.println(Thread.currentThread().getName() + " : 唤醒 method1 和 method3");
+            System.out.println(Thread.currentThread().getName() + " : condition1 唤醒 method1 和 method3");
+            Thread.sleep(2000);
             condition1.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
             lock.unlock();
         }
@@ -72,8 +80,11 @@ public class ReentrantLockExample {
     public void method5(){
         try {
             lock.lock();
-            System.out.println(Thread.currentThread().getName() + " : 唤醒 method2");
+            System.out.println(Thread.currentThread().getName() + " : condition2 唤醒 method2");
+            Thread.sleep(2000);
             condition2.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
             lock.unlock();
         }
@@ -111,6 +122,8 @@ public class ReentrantLockExample {
             }
         });
 
+        //t1先启动，t1先拿到锁，t1执行完成之后 t1 condition1进入等待，然后释放锁t1进入等待阶段
+        //t2拿到锁，t2执行后，t2 condition进入等待，释放锁 t2进入等待阶段
         t1.start();t2.start();t3.start();
 
         Thread t4 = new Thread(new Runnable() {
@@ -133,11 +146,14 @@ public class ReentrantLockExample {
                 r.method6();
             }
         });
+        //
         t6.start();
 
-        Thread.sleep(2000);
+        Thread.sleep(4000);
+        //t4 拿到锁后，唤醒condition1 的线程，t1、t3继续执行，执行完成之后释放锁
         t4.start();
         Thread.sleep(2000);
+        //t5 拿到锁后，唤醒condition2 的线程，t2继续执行
         t5.start();
     }
 }
