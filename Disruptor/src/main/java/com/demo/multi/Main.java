513bfd6b1d18e7c022ffd32a0b260df50c119129
hongxudong <hongxudong@huoniu360.com>
2017-04-10 23:01:05 +0800

    添加多ä生产者和消费者 调用的demo“”

--- Disruptor/src/main/java/com/demo/multi/Main.java
+++ Disruptor/src/main/java/com/demo/multi/Main.java
@@ -4,10 +4,7 @@ import com.lmax.disruptor.*;
 import com.lmax.disruptor.dsl.ProducerType;
 
 import java.util.UUID;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * @Author: hongxudong@lxfintech.com
@@ -19,22 +16,27 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         //创建事件发布管理器
-        RingBuffer<Order> ringBuffer = RingBuffer.create(ProducerType.SINGLE, new EventFactory<Order>() {
+        // RingBuffer: 被看作disruptor中最主要的组件。然而3.0之后开始，RingBuffer仅仅负责存储和更新的disruptor中流通
+        // 的数据。对一些特殊的应用场景可以被用户（使用其他数据结构）完全替代
+        //ProducerType.MULTI : 如果生产者是多线程的话，这个地方一定要设置为MULTI 多线程模式，否则会出现漏掉没执行的问题
+        RingBuffer<Order> ringBuffer = RingBuffer.create(ProducerType.MULTI, new EventFactory<Order>() {
             @Override
             public Order newInstance() {
                 return new Order();
             }
-        }, 1024, new YieldingWaitStrategy());
-        //用于管理多个消费者，维护序列
+        }, 1024*1024, new YieldingWaitStrategy());
+
+        //  SequenceBarrier：由Sequence生成，并且包含了已经发布的Sequence的引用，这些Sequence源于Sequencer和一些独立的
+        //  消费者的Sequence。它包含了决定是否有供消费者来消费的Event的逻辑
         SequenceBarrier sequenceBarrier = ringBuffer.newBarrier();
 
         //创建多个消费者
         Consumer[] consumers = new Consumer[3];
-        for (int i = 0; i < 3; i++) {
-            consumers[i] = new Consumer("consumer"+i);
+        for (int i = 0; i < consumers.length; i++) {
+            consumers[i] = new Consumer("c"+i);
         }
-
-        //工作池，类似于线程池？？
+        //WorkProcessor : 确保每个sequence只被一个processor消费，在同一个workPool中处理的多个workProcessor不会消费同样的sequence
+        //WorkPool： 一个workProcessor池，其中workProcessor将消费sequence，所以任务可以在实现workHandler接口的worker直接移交
         WorkerPool workerPool = new WorkerPool(ringBuffer, sequenceBarrier, new ExceptionHandler() {
             @Override
             public void handleEventException(Throwable ex, long sequence, Object event) {
@@ -51,11 +53,16 @@ public class Main {
                 ex.printStackTrace();
             }
         }, consumers);
-        //????  这个貌似很重要啊？
+        //TODO 这句话干嘛的啊？？  这个貌似很重要啊？
+        // workerPool.getWorkerSequences()： 获取生产者对应的三个sequence
+        //addGatingSequences ： 设置生产者根据各自对应的sequence 调节各自对应的速度，维持生产者和消费者的顺序执行
         ringBuffer.addGatingSequences(workerPool.getWorkerSequences());
-        workerPool.start(Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()));
+        //创建线程池
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        //启动 workPool
+        workerPool.start(executorService);
 
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(100);
+        final CyclicBarrier cyclicBarrier = new CyclicBarrier(100);
         for(int i = 0; i < 100 ; i++){
             final Producer p = new Producer(ringBuffer);
             new Thread(new Runnable() {
@@ -74,9 +81,8 @@ public class Main {
                 }
             }).start();
         }
-
         System.out.println("----------程序开始执行-------------");
-        Thread.sleep(6000);
+        Thread.sleep(5000);
         System.out.println("total: " + consumers[0].getCount());
     }
 }
