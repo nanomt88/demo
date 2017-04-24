6d61b0c7c1bf1cb5044c0377d2659b7dd201e99f
hongxudong <hongxudong@huoniu360.com>
2017-04-24 08:19:09 +0800

    å¤å¤添加 websocket demo

---
+++ Netty/src/main/java/netty/websocket/WebSocketServer.java
@@ -0,0 +1,50 @@
+package netty.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+/**
+ * Created by ZBOOK-17 on 2017/4/21.
+ */
+public class WebSocketServer {
+
+    public static void main(String[] args) {
+
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+
+        try{
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast("http-codec", new HttpServerCodec());
+                            pipeline.addLast("aggregator", new HttpObjectAggregator(65536));
+                            pipeline.addLast("http-chunked", new ChunkedWriteHandler());
+                            pipeline.addLast("handler", new WebsocketServerHandler());
+                        }
+                    });
+
+            ChannelFuture channelFuture = bootstrap.bind(8080).sync();
+            System.out.println("Web socket server started, port 8080...");
+            System.out.println("visit at http://localhost:8080/");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            worker.shutdownGracefully();
+            boss.shutdownGracefully();
+        }
+
+    }
+}
