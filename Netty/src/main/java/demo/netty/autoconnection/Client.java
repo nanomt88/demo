51a06ce88293d7dd6283f18df6dbc9e501e6a369
洪旭东 <hongxudong@lxfintech.com>
2017-04-29 15:23:59 +0800

    添加客户端自动重连机制

---
+++ Netty/src/main/java/demo/netty/autoconnection/Client.java
@@ -0,0 +1,141 @@
+package demo.netty.autoconnection;
+
+import demo.netty.util.GzipUtils;
+import demo.netty.util.MarshallingCodeFactory;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by ZBOOK-17 on 2017/4/20.
+ */
+public class Client {
+
+    EventLoopGroup group = null;
+    Bootstrap bootstrap = null;
+    ChannelFuture future = null;
+
+    private int port ;
+    private String ip;
+    public Client(String ip, int port){
+        this.port = port;
+        this.ip = ip;
+        group = new NioEventLoopGroup();
+        bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .option(ChannelOption.SO_SNDBUF, 32*1024)
+                .option(ChannelOption.SO_RCVBUF, 32*1024)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(MarshallingCodeFactory.buildMarshallingEncoder());
+                        pipeline.addLast(MarshallingCodeFactory.buildMarshallingDecoder());
+                        pipeline.addLast(new ClientHandle());
+                    }
+                });
+
+    }
+    public void connect(){
+        try {
+            future = bootstrap.connect(ip, port).sync();
+            System.out.println("连接到远程服务器："+ip+" : "+ port);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ChannelFuture getFutureChannel(){
+        if(future == null){
+            synchronized (this) {
+                if(future == null) {
+                    connect();
+                }
+            }
+        }
+        if(!future.channel().isActive()){
+            synchronized (this) {
+                if(!future.channel().isActive()) {
+                    connect();
+                }
+            }
+        }
+        return future;
+    }
+
+    public static void main(String[] args) {
+        final Client client = new Client("127.0.0.1",8080);
+
+        try {
+            for(int i=0 ; i<5; i++){
+                RequestObject req = new RequestObject();
+                req.setId(""+i);
+                req.setName("pic"+i);
+                req.setRequestMessage("我是client："+i);
+
+                client.getFutureChannel().channel().writeAndFlush(req);
+                TimeUnit.SECONDS.sleep(3);
+            }
+            client.getFutureChannel().channel().closeFuture().sync();
+
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    System.out.println(Thread.currentThread().getName()+"   进入子线程....");
+                    ChannelFuture f = client.getFutureChannel();
+                    System.out.println(Thread.currentThread().getName()+ "  isActive:"+ f.channel().isActive());
+                    System.out.println(Thread.currentThread().getName()+ "  isOpen:"+ f.channel().isOpen());
+
+                    RequestObject req = new RequestObject();
+                    req.setId("sub99");
+                    req.setName("pic99");
+                    req.setRequestMessage("我是client：99");
+
+                    try {
+
+                        f.channel().writeAndFlush(req);
+                        f.channel().closeFuture().sync();
+                        System.out.println(Thread.currentThread().getName()+"   关闭子线程");
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+            }).start();
+
+            System.out.println("断开连接,主线程结束..");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+
+        }
+    }
+
+    private static class ClientHandle extends SimpleChannelInboundHandler<ResponseObject> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, ResponseObject msg) throws Exception {
+            System.out.println(msg);
+        }
+
+        @Override
+        public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+            super.channelUnregistered(ctx);
+            System.out.println("连接关闭。。。");
+        }
+    }
+}
---
+++ Netty/src/main/java/demo/netty/autoconnection/Server.java
@@ -0,0 +1,93 @@
+package demo.netty.autoconnection;
+
+import demo.netty.util.GzipUtils;
+import demo.netty.util.MarshallingCodeFactory;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * Created by ZBOOK-17 on 2017/4/19.
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            //创建线程组
+
+            //创建服务器辅助类
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(boss,worker)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childOption(ChannelOption.SO_BACKLOG, 1024)
+                    .childOption(ChannelOption.SO_SNDBUF, 32*1024)
+                    .childOption(ChannelOption.SO_RCVBUF, 32*1024)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            //设置Marshalling编解码器
+                            ch.pipeline().addLast(MarshallingCodeFactory.buildMarshallingDecoder());
+                            ch.pipeline().addLast(MarshallingCodeFactory.buildMarshallingEncoder());
+                            ch.pipeline().addLast(new ReadTimeoutHandler(5));
+                            ch.pipeline().addLast(new ServerHandle());
+                        }
+                    });
+            //绑定端口
+            ChannelFuture future = bootstrap.bind(8080).sync();
+
+            //等待服务器监听端口关闭
+            future.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            //关闭线程组  释放资源
+            worker.shutdownGracefully();
+            boss.shutdownGracefully();
+        }
+    }
+
+
+    private static class ServerHandle extends SimpleChannelInboundHandler<RequestObject> {
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("Client:"+ctx.channel().remoteAddress()+" active...");
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            cause.printStackTrace();
+            //关闭连接
+            ctx.close();
+        }
+
+        /**
+         * SimpleChannelInboundHandler 的channelRead0方法，会自动清空缓存
+         * @param ctx
+         * @param msg
+         * @throws Exception
+         */
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, RequestObject msg) throws Exception {
+            System.out.println("收到消息：：："+msg);
+            ResponseObject response = new ResponseObject();
+            response.setId("ID:"+msg.getId());
+            response.setName("Name:"+msg.getName());
+            response.setResponseMessage("ResponseMessage:"+msg.getRequestMessage());
+            ctx.channel().writeAndFlush(response);
+        }
+    }
+}
