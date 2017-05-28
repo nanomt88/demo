719514dae3475fa4fcec131855cdd5bf6325ce1c
hongxudong <hongxudong@huoniu360.com>
2017-05-28 22:44:52 +0800

    添加 master-slave 方式连接

---
+++ activemq/src/main/java/com/nanomt88/demo/activemq/cluster/ProductDemo.java
@@ -0,0 +1,56 @@
+package com.nanomt88.demo.activemq.cluster;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.ActiveMQSession;
+
+import javax.jms.*;
+
+/**
+ * Created by ZBOOK-17 on 2017/5/26.
+ */
+public class ProductDemo {
+
+    //private  static final Logger logger = LoggerFactory.getLogger(ProductDemo.class);
+
+    public static void main(String[] args) throws JMSException {
+
+        //1. 建立ConnectionFactory工厂对象，填入用户名、密码、已经要连接的地址
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnectionFactory.DEFAULT_USER,
+                ActiveMQConnectionFactory.DEFAULT_PASSWORD,
+                "failover:(tcp://192.168.1.130:61611,tcp://192.168.1.130:61612,tcp://192.168.1.130:61613)?Randomize=false");
+
+        //2. 通过ConnectionFacory对象创建一个Connection连接，并且调用start方法开启连接，Connection默认是关闭状态
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        //3. 通过Connection连接创建Session会话（上下文环境对象），用于接受消息和发送消息。
+        // 第一个参数：true 启用事物； 第二个参数：签收模式，一般设置为自动签收
+        Session session = connection.createSession(false, ActiveMQSession.AUTO_ACKNOWLEDGE);
+
+        //4. 通过session 创建 Destination对象，作用：一个客户端用来指定生产消息目标和消费者消息来源的对象。
+        //PTP模式中，destination被称之为 队列即 Queue
+        //在Pub/sub模式中，Destination被称为Topic即主题。程序中可以指定多个Queue和Topic
+        Destination destination = session.createQueue("cluster-queue");
+
+        //5. 通过session创建消息的发送和接受对象（生产者和消费者）： MessageProduct、 MessageConsumer
+        MessageProducer producer = session.createProducer(destination);
+
+        //6. 可以使用 MessageProducer的setDeliveryMode 方法设置消息是否需要持久化
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+        //7. 最后使用JMS规范的TextMessage形式创建数据对象，并用MessageProduct对象send方法发送数据。
+        //     消费端使用receive方法接受数据。最后要记得关闭connection
+        for (int i = 0; i < 5; i++) {
+            TextMessage message = session.createTextMessage();
+            message.setText("hello world 。。。 "+ i);
+
+            producer.send(message);
+            //logger.info("发送消息:{}", message);
+            System.out.println("发送消息：" + i);
+        }
+
+        if(session != null) {
+            connection.close();
+        }
+    }
+}
