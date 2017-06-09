ad3e8c9df068564141674b0c3cdf9728c1182b55
nanomt88 <nanomt88@gmail.com>
2017-06-09 08:28:26 +0800

    添加顺序消费的demo

--- rocketmq/src/main/java/com/nanomt88/demo/rocketmq/order/Consumer.java
+++ rocketmq/src/main/java/com/nanomt88/demo/rocketmq/order/Consumer.java
@@ -1,7 +1,88 @@
 package com.nanomt88.demo.rocketmq.order;
 
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.*;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
 /**
- *  顺序消费场景 demo
+ * 顺序消费场景 demo
  */
 public class Consumer {
+
+    private  static final Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+    public static void main(String[] args) throws MQClientException {
+
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("ProducerGroupName");
+
+        consumer.setNamesrvAddr("192.168.1.140:9876;192.168.1.141:9876");
+
+
+
+        /**
+         * 设置 在Push模式下，一次最多拉取的消息数量
+         */
+        consumer.setConsumeMessageBatchMaxSize(1);
+
+        /**
+         *
+         *  setPullBatchSize 方法在 DefaultMQPullConsumer 方式下才会生效
+         *  consumer.setPullBatchSize(32);
+         */
+        consumer.setPullInterval(0);
+
+        // ============================== consumer 配置参数结束 =====================================
+
+
+        consumer.subscribe("TopicTest1", "*");
+        //consumer.subscribe("TopicTest2", "*");
+        //consumer.subscribe("TopicTest3", "*");
+
+        //注册从broker读取到消息之后，处理消息的回调事件
+        //顺序消费必须实现：MessageListenerOrderly接口，这个接口保证单线程消费
+        consumer.registerMessageListener(new MessageListenerOrderly() {
+
+            @Override
+        public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
+                /**
+                 *  List<MessageExt> msgs : 参数msgs为list类型，可以进行批量拉取消息
+                 *
+                 */
+                try {
+                    /**
+                     *  这里处理业务逻辑，业务逻辑处理成功之后才返回 CONSUME_SUCCESS
+                     */
+                    if(msgs!=null && msgs.size()>0){
+
+                        MessageExt msg = msgs.get(0);
+                        String body = new String(msg.getBody(),"UTF-8");
+                        logger.info("{} 收到消息：tag:{}msg:{}" , new Object[] { body ,msg.getTags(),  msg});
+
+                        Thread.sleep(500);
+                        logger.info("{} 执行完毕", body);
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    /**
+                     * 如果消息出现异常，就返回失败，下次会重新消费这一条消息
+                     */
+                    return ConsumeOrderlyStatus.SUSPEND_CURRENT_QUEUE_A_MOMENT;
+                }
+                return ConsumeOrderlyStatus.SUCCESS;
+            }
+        });
+
+        /*
+         *  启动
+         */
+        consumer.start();
+
+        System.out.printf("Consumer Started.%n");
+    }
 }
--- rocketmq/src/main/java/com/nanomt88/demo/rocketmq/order/Producer.java
+++ rocketmq/src/main/java/com/nanomt88/demo/rocketmq/order/Producer.java
@@ -1,8 +1,104 @@
 package com.nanomt88.demo.rocketmq.order;
 
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  *  顺序消费场景 demo
  */
-public class Producer
-{
+public class Producer {
+
+    public static void main(String[] args) throws MQClientException {
+        /**
+         * 一个应用创建一个Producer，由应用来维护此对象，可以设置为全局对象或者单例<br>
+         * 注意：ProducerGroupName需要由应用来保证唯一<br>
+         * ProducerGroup这个概念发送普通的消息时，作用不大，但是发送分布式事务消息时，比较关键，
+         * 因为服务器会回查这个Group下的任意一个Producer
+         */
+        DefaultMQProducer producer = new DefaultMQProducer("ProducerGroupName");
+
+        producer.setNamesrvAddr("192.168.1.140:9876;192.168.1.141:9876");
+        /**
+         * Producer对象在使用之前必须要调用start初始化，初始化一次即可<br>
+         * 注意：切记不可以在每次发送消息时，都调用start方法
+         */
+        producer.start();
+
+        /**
+         * 下面这段代码表明一个Producer对象可以发送多个topic，多个tag的消息。
+         * 注意：send方法是同步调用，只要不抛异常就标识成功。但是发送成功也可会有多种状态，<br>
+         * 例如消息写入Master成功，但是Slave不成功，这种情况消息属于成功，但是对于个别应用如果对消息可靠性要求极高，<br>
+         * 需要对这种情况做处理。另外，消息可能会存在发送失败的情况，失败重试由应用来处理。
+         */
+        for (int i = 0; i < 4; i++){
+            try {
+                {
+                    Message msg = new Message("TopicTest1",// topic
+                            "order_1",// tag
+                            "OrderID-A00"+i,// key ： 用于唯一标记，方便去重
+                            (new Date().toString() + "message_A"+i).getBytes());// body
+                    //发送数据： 如果使用顺序消费，必须直接自己实现MessageQueueSelector接口，保证消息发送到同一个队列
+                    SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                        @Override
+                        public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                            // 参数arg是外层传入的0，用于选择队列
+                            Integer index = (Integer) arg;
+                            return mqs.get(index);
+                        }
+                    },0 );  //0 : 表示队列的下标
+                    System.out.println(sendResult);
+                }
+
+                {
+                    Message msg = new Message("TopicTest1",// topic
+                            "order_2",// tag
+                            "OrderID-B00"+i,// key
+                            (new Date().toString() + "message_B"+i).getBytes());// body
+                    SendResult sendResult = producer.send(msg);
+                    System.out.println(sendResult);
+                }
+
+                {
+                    Message msg = new Message("TopicTest1",// topic
+                            "order_3",// tag
+                            "OrderID-C00"+i,// key
+                            (new Date().toString() + "message_C"+i).getBytes());// body
+                    SendResult sendResult = producer.send(msg);
+                    System.out.println(sendResult);
+                }
+
+                {
+                    Message msg = new Message("TopicTest1",// topic
+                            "order_4",// tag
+                            "OrderID-D00"+i,// key
+                            (new Date().toString() + "message_D"+i).getBytes());// body
+                    SendResult sendResult = producer.send(msg);
+                    System.out.println(sendResult);
+                }
+            } catch (RemotingException e) {
+                e.printStackTrace();
+            } catch (MQBrokerException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        /**
+         * 应用退出时，要调用shutdown来清理资源，关闭网络连接，从MetaQ服务器上注销自己
+         * 注意：我们建议应用在JBOSS、Tomcat等容器的退出钩子里调用shutdown方法
+         */
+        producer.shutdown();
+
+    }
 }
