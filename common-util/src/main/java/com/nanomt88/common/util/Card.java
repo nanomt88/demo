f1f5527698ccd95c3ba43fb8a725ce5bbc9adbf2
hongxudong <hongxudong@huoniu360.com>
2017-07-07 07:54:11 +0800

    完善二分查找算法

--- common-util/src/main/java/com/nanomt88/common/util/Card.java
+++ common-util/src/main/java/com/nanomt88/common/util/Card.java
@@ -1,9 +1,7 @@
 package com.nanomt88.common.util;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author nanomt88@gmail.com
@@ -15,6 +13,8 @@ public class Card {
 
     private static BigInteger[] MAIN_ACCOUNT = null;
 
+    private static Set<String> SET = new HashSet<>(5000*3);
+
     public static synchronized void init(String[] array){
         List<BigInteger> list = new ArrayList<BigInteger>();
         for(String cardNO : array){
@@ -22,12 +22,15 @@ public class Card {
             if(no != null){
                 list.add(no);
             }
+            //
+            SET.add(cardNO.replaceAll("F",""));
         }
 
         MAIN_ACCOUNT = list.toArray(new BigInteger[list.size()]);
 
         //按照大小重新排序
         Arrays.sort(MAIN_ACCOUNT);
+
     }
 
     public static String searchCardBin(String cardNo){
@@ -36,10 +39,41 @@ public class Card {
             return null;
         }
         //卡号在最大最小区间内
-        if(integer.compareTo(MAIN_ACCOUNT[0]) == -1 || integer.compareTo(MAIN_ACCOUNT[MAIN_ACCOUNT.length-1]) == 1 ){
+        if(integer.compareTo(MAIN_ACCOUNT[MAIN_ACCOUNT.length-1]) == 1 ){
             return null;
         }
+        if(integer.compareTo(MAIN_ACCOUNT[0]) == -1){
+            String card = toHexString(MAIN_ACCOUNT[0]);
+            if(cardNo.startsWith(card.replace("F",""))){
+                return card;
+            }else {
+                return null;
+            }
+        }
+        int[] indexs = binarySearch(integer);
+        return verify(cardNo, indexs[0]);
+    }
 
+    private static String verify(String cardNo, int indexs){
+
+        for(int j=indexs; j<MAIN_ACCOUNT.length ; j++) {
+            String origin = toHexString(MAIN_ACCOUNT[j]);
+
+            if(origin.length() != cardNo.length()){
+                return null;
+            }
+            for (int i = 0; i < origin.length(); i++) {
+                if (origin.charAt(i) == 'F') {
+                    if(j-indexs > 0 )System.out.println("     match in : " + (j-indexs));
+                    return origin;
+                }
+                if (origin.charAt(i) == cardNo.charAt(i)) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
         return null;
     }
 
@@ -50,7 +84,7 @@ public class Card {
     private static int[] binarySearch(BigInteger cardNo){
         int low = 0;
         int high = MAIN_ACCOUNT.length-1;
-        while (low <= high){
+        for ( int j=0 ; low <= high; j++){
             int middle = (low + high)/2;
             if( cardNo.compareTo(MAIN_ACCOUNT[middle]) == 1){   //当前数大于中间数，低位数增大
                 low = middle ;
@@ -58,11 +92,15 @@ public class Card {
                 high = middle ;
             }   //卡号不存在等于卡bin的情况，所以不需要判断
             if( high - low == 1){       //如果低位和高位刚好相差1位，则cardNo落在这两个数字中间
-                return new int[]{low, high};
+//                return new int[]{low, high};
+                System.out.println("循环次数：" + j);
+                return new int[]{high};
             }else if( high == low){     //如果低位和高位相等
                 int small = middle > high ? low : middle;
                 int big = middle > high ? middle : high;
-                return new int[]{small, big};
+//                return new int[]{small, big};
+                System.out.println("循环次数：" + j);
+                return new int[]{big};
             }
         }
 
@@ -98,13 +136,87 @@ public class Card {
         return integer.toString(HEX_RADIX).toUpperCase();
     }
 
-    public static void main(String[] args) {
+    /**
+     * 构造真实卡号
+     * @param random
+     * @param card
+     * @return
+     */
+    public static String cloneCard(Random random, String card){
+        char[] chars = new char[card.length()];
+        for (int i = 0; i < card.length(); i++) {
+            chars[i] = card.charAt(i);
+            if(chars[i] == 'F'){
+                chars[i] =  (random.nextInt(9)+"").charAt(0);
+            }
+        }
+        //构造真实卡号
+        String realCard = new String(chars, 0, chars.length);
+        return realCard;
+    }
 
+    public static String setSearch(String card){
+        for(int i=10; i>1; i--){
+            String cardBin = card.substring(0,i);
+            if(SET.contains(cardBin)){
+                return cardBin;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        BigInteger i1 = hexToBigInteger("9FFFFFFFFFFFFFFF");
+        System.out.println(i1);
+        BigInteger i2 = hexToBigInteger("1FFFFFFFFFF");
+        System.out.println(i2);
+        System.out.println(i1.subtract(i2));
+//        System.out.println("123456789012".substring(0,10));
+//        testAll();
+//        test1();
+
+    }
+
+    private static void testAll(){
         init(Test.cards);
-        long time = System.currentTimeMillis();
-        int[] ints = binarySearch(hexToBigInteger("6236869999999999999"));
-        long end = System.currentTimeMillis();
-        System.out.println("耗时：" + (end - time));
-        System.out.println(toHexString(MAIN_ACCOUNT[ints[0]]) + "," + toHexString(MAIN_ACCOUNT[ints[1]]));
+
+        Random r = new Random();
+
+        long count1 = 0;
+        long count2 = 0;
+        for(String str : Test.cards){
+
+            String cardNo = cloneCard(r, str);
+
+            long start = System.nanoTime();
+            String cardBin = searchCardBin(cardNo);
+            long end = System.nanoTime();
+            long time1= end - start; count1 = time1+count1;
+
+            long start2 = System.nanoTime();
+            String cardBin2 = setSearch(cardNo);
+            long end2 = System.nanoTime();
+            long time2 = end2 - start2;count2 = time2+count2;
+
+            System.out.println("CardNo: "+ cardNo  +"   耗时二分："+ time1
+                    +"   耗时Map："+ time2 + "  是否相等：" + cardBin.replaceAll("F","").equals(cardBin2));
+
+        }
+        System.out.println("总耗时对比， 二分："+ count1 + "  Map：" + count2);
+        System.out.println("平均耗时对比， 二分："+ count1/Test.cards.length + "  Map：" + count2/Test.cards.length);
+    }
+
+    private static void test1(){
+        init(Test.cards);
+
+        Random r = new Random();
+
+        String cardNo = "4041748162286777";  //CardNo: 6011267858055075 期望：60112FFFFFFFFFFF  实际：601126FFFFFFFFFF
+//        String cardNo = cloneCard(r, "60112FFFFFFFFFFF");  //CardNo: 6011267858055075 期望：60112FFFFFFFFFFF  实际：601126FFFFFFFFFF
+
+        long time1 = System.nanoTime();
+        String cardBin = searchCardBin(cardNo);
+        long time2 = System.nanoTime();
+        System.out.println( "cardNo: " + cardNo + "  耗时: " + (time2 - time1));
     }
 }
--- common-util/src/main/java/com/nanomt88/common/util/asm/TestObject.java
+++ common-util/src/main/java/com/nanomt88/common/util/asm/TestObject.java
@@ -7,8 +7,34 @@ import java.util.Random;
 import static com.nanomt88.common.util.asm.SizeOfObject.*;
 
 /**
+ *  添加maven打包配置，如下：
+ *
+ *      打jar相关的配置
+         <plugin>
+         <groupId>org.apache.maven.plugins</groupId>
+         <artifactId>maven-jar-plugin</artifactId>
+         <version>2.4</version>
+         <configuration>
+         <finalName>test</finalName>
+         <archive>
+         <manifest>
+         <addClasspath>true</addClasspath>
+         <classpathPrefix>lib/</classpathPrefix>
+         <mainClass>com.nanomt88.common.util.asm.TestObject</mainClass>
+         </manifest>
+         <manifestEntries>
+         <Premain-class>com.nanomt88.common.util.asm.SizeOfObject</Premain-class>
+         <Boot-Class-Path></Boot-Class-Path>
+         <Can-Redefine-Classes>false</Can-Redefine-Classes>
+         </manifestEntries>
+         <addMavenDescriptor>false</addMavenDescriptor>
+         </archive>
+         </configuration>
+         </plugin>
+ *
  *  先使用mvn clean package打包
- *  然后到target目录下面，运行指定的jar包，命令如下：java -javaagent:test.jar.jar -cp . com.nanomt88.common.util.asm
+ *  然后到target目录下面，运行指定的jar包，命令如下：
+ *          java -javaagent:test.jar -cp . com.nanomt88.common.util.asm.TestObject
  *
  * @author nanomt88@gmail.com
  * @create 2017-07-01 9:19
