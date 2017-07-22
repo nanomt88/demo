07361fa7b874dce293b96da8e77e0d318a337b5b
hongxudong <hongxudong@huoniu360.com>
2017-07-22 17:08:59 +0800

    添加 curator 多client 的示例

---
+++ zookeeper/src/main/java/com/nanomt88/demo/curator/cluster/Client2.java
@@ -0,0 +1,12 @@
+package com.nanomt88.demo.curator.cluster;
+
+
+public class Client2 {
+
+	public static void main(String[] args) throws Exception{
+		
+		CuratorWatcher watcher = new CuratorWatcher();
+		System.out.println("client2 started...");
+		Thread.sleep(100000000);
+	}
+}
