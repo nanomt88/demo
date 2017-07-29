5e03ba53faff517bdc7aba8c6af05055c20529f9
洪旭东 <hongxudong@lxfintech.com>
2017-07-29 09:34:20 +0800

    添加

---
+++ Netty/src/main/java/demo/client/LruCache.java
@@ -0,0 +1,48 @@
+package demo.client;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @Author: hongxudong@lxfintech.com
+ * @Created: 2017/7/27 обнГ8:39
+ * @Description: //TODO
+ */
+
+public class LruCache<K, V> extends LinkedHashMap<K, V> {
+    private int cacheSize;
+
+    public LruCache(int cacheSize) {
+        super(cacheSize, 0.75F, true);
+        this.cacheSize = cacheSize;
+    }
+
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() >= cacheSize;
+    }
+
+    public static void main(String[] args) {
+        String flag="100-";
+        LruCache<String,Integer> cache=new LruCache(1000);
+        for (int i=0;i<10000;i++){
+            cache.put(i+"-",i);
+            if(i%100==0){
+                System.out.print(cache.get(flag)+",");
+            }
+        }
+        System.out.println();
+        System.out.println(cache.get(flag));
+        System.out.println(cache.get(flag));
+        System.out.println(cache.get(flag));
+//        client.put(1000+"",1002222);
+//        System.out.println(client.get(flag));
+//        System.out.println(client.get(flag));
+        Iterator it=cache.keySet().iterator();
+        while (it.hasNext()){
+            System.out.print(it.next());
+            System.out.print(",");
+        }
+
+    }
+}
