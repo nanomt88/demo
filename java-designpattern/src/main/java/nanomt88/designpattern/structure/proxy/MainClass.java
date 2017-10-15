fe5049fc93a16887d0e5dd47928a14bca11b3953
hongxudong <hongxudong@huoniu360.com>
2017-10-15 09:35:56 +0800

    添加代理模式

---
+++ java-designpattern/src/main/java/nanomt88/designpattern/structure/proxy/MainClass.java
@@ -0,0 +1,12 @@
+package nanomt88.designpattern.structure.proxy;
+
+/**
+ * @author nanomt88@gmail.com
+ * @create 2017-10-15 9:35
+ **/
+public class MainClass {
+    public static void main(String[] args) {
+        ProxyObject proxyObject = new ProxyObject();
+        proxyObject.operation();
+    }
+}
